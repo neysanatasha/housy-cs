263bf8bbfe03b85f8cdaabd1216310d580cd432a
MACBOOK <PRO@MOJAVEs-MacBook-Pro.local>
2023-02-14 10:26:35 +0700

    revisi lagi

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:8000/api/v1/",
+  baseURL: "http://housy-cs.up.railway.app/api/v1",
 });
 
 export const setAuthToken = (token) => {
